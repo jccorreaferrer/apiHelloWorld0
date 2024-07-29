8615b26211a008b9dade0f726bab5015194cfc7e
WSP International <WSP International@DESKTOP-V2V28IJ>
2024-07-29 08:42:21 -0600

    Add project files.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apiHelloWorld0.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet(Name = "HelloWorld")]
+        public String Get()
+        {
+            return  "Hello World";
+        }
+    }
+}
